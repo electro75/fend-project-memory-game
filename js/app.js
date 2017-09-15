0c01804406f5ec7a091d72159513a78796fa2dc1
Sahil Naikdhure <sanaikdhure@gmail.com>
2017-09-16 04:43:18 +0530

    Some progress?
    Timer added, Game starts on clicking start button. Clicking still needs fixing.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,86 @@
+$(function(){
 /*
  * Create a list that holds all of your cards
  */
-var cardList=[];
-
+var cardList=["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt",
+              "fa fa-cube","fa fa-anchor","fa fa-leaf","fa fa-bicycle",
+               "fa fa-diamond","fa fa-bomb","fa fa-leaf","fa fa-bomb",
+               "fa fa-bolt","fa fa-bicycle","fa fa-paper-plane-o","fa fa-cube"];
+var time=0;
+var openList=[];
+var matched=0;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+function displayShuffled(array){
+    var i=0;
+    var len=array.length;
+    var currentCard=$('.deck').children().first();
+    for(i=0;i<len;i++){
+        currentCard.append("<i></i>");
+        currentCard.children().addClass(array[i]);
+        currentCard=currentCard.next();
+    }
+    return;
+};
 function displayBlank(){
-    $('.deck').children().removeClass("match show open");
+    $('.card').removeClass("match show open");
+    return ;
+};
+function timerStart(){
+    var id = setInterval(function(){ 
+    time++; 
+    $('#timer').children().text(time+"s");
+    
+   },1000);
+    return;
+};
+function displayCard(evt){
+    $(evt.target).addClass("show open");
+    return;
+}
+function check(e){
+    var selected=$(e.target).children();
+    console.log(openList.length)
+    if(openList.length==1){
+        if(openList[0]==selected){
+            $('.show').addClass('match');
+            $('.show').removeClass('open');
+            matched++;
+        }
+        else{
+            $('.show').removeClass("show open");
+        }
+        openList=[];
+    }
+    else{
+        openList[0]=selected;
+    }
+return;
 }
 function openForFive(){
-    // var timeLeft=10;
-    // var startTimer=setInterval(function(){
-    //   timeLeft--;
-    //   $('.deck').children().addClass("show");
-    //   $('.glyphicons-alarm').val(timeLeft);
-    //   if(timeLeft<=0){
-    //     displayBlank();
-    //     clearInterval(startTimer);
-    //   },1000);
-    $('glyphicons-alarm').addClass("show");
+    $('.deck').children().addClass("show");
+    var timeleft = 8;
+    var shuffledDeck= shuffle(cardList);
+    displayShuffled(shuffledDeck);
+    var startTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdown").textContent = " "+timeleft;
+    if(timeleft <= 0){
+
+        clearInterval(startTimer);
+        displayBlank();
+        timerStart();
+        
+    }
+    },1000);
+    return;
     };
-}
+
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -51,9 +107,16 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
- $('button').on('click',function(){
+ $('.btn').on('click',function(){
    openForFive();
- })
+ });
  $('.fa-repeat').on('click',function(){
    displayBlank();
  });
+ $('.card').on('click',function(e){
+    displayCard(e);
+    check(e);
+});
+
+
+});
